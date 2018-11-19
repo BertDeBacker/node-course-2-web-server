f2359d85ef52757b4daf9faab9b3b0a7c5b27ec9
Bert De Backerk <bertdebackerkoninkx@gmail.com>
2018-11-19 09:58:28 +0100

    added Web Learning journey

--- server.js
+++ server.js
@@ -161,70 +161,96 @@ app.get('/Programming', (req, res) => {
             usefullLinks: `http://${reqHost}//${usefullLinks}`,
             helpPage: `http://${reqHost}//${helpPage}`,
             pageLinks: [{
-                link: 'https://www.udemy.com/the-complete-nodejs-developer-course-2/',
-                text: 'Udemy, NodeJS developer course',
-                description: 'odeJS interactive training with teachter. <br> This training was purchased in October 2018.'
-            }, {
-                link: 'https://www.npmjs.com/',
-                text: 'npm',
-                description: 'Library containing thousents of open source building blocks. <br> These blocks are free to use in your application.'
-            }, {
-                link: 'http://expressjs.com/',
-                text: 'express',
-                description: 'Library facilitating webserver framework. <br> Extensive learning, help and documentation can be found on http://expressjs.com/'
-            }, {
-                link: 'http://handlebarsjs.com/',
-                text: 'Handlebars',
-                description: 'Library for dynamic html (npm install hbs)'
-            }, {
-                link: 'https://medium.freecodecamp.org/the-complete-javascript-handbook-f26b2c71719c',
-                text: 'Complete JavaScript handbook',
-                description: 'Free online book about JavaScript.'
-            }, {
-                link: 'https://help.github.com/',
-                text: 'GitHub Help website',
-                description: 'Free online book about JavaScript.'
-            }, {
-                link: 'https://mongodb.github.io/node-mongodb-native/',
-                text: 'MongoDB',
-                description: 'MongoDB and native MongoDB driver for NodeJS'
-            }, {
-                link: 'https://mongoosejs.com/',
-                text: 'Mongoose',
-                description: 'Library for mongodb data manipulation for NodeJS'
-            }, {
-                link: 'https://dashboard.heroku.com',
-                text: 'Heroku',
-                description: 'To publish NodeJs websites'
-            }, {
-                link: 'https://github.com/',
-                text: 'Github',
-                description: 'To publish NodeJs websites'
-            }, {
-                link: 'https://www.w3schools.com/howto/howto_js_full_page_tabs.asp',
-                text: 'w3school',
-                description: 'Website with all kid of web design reference materials'
-            }, {
-                link: 'https://www.getpostman.com/',
-                text: 'postman',
-                description: 'Postman is a toolbox for testing and monitoring API\'s.'
-            }, {
-                link: "https://httpstatuses.com/",
-                text: 'https statuses',
-                description: 'All https statuses that exist'
-            }, {
-                link: 'http://blog.getpostman.com/2018/04/11/first-5-things-to-try-if-youre-new-to-postman/?mkt_tok=eyJpIjoiTnpZeE4ySTRObVV6WWpNdyIsInQiOiJZUEtrQnFtVjRCcTdsWllTVXN2ZmhnMFc2cWhGNjZqdmtQRlduRGZmeEpYTTRGTGxxa0N6T3IraEVGSUF3d3g0RW5cL1FYbUNuUUh5Sm5OaFJyWjNoSG9vOHFVT2xYalR4SjY4MmdrS0pQMG5JUmpKMlplSDBBSDQxdXRjUFg0ZzAifQ%3D%3D',
-                text: 'Try Postmans, 5 things to try out.',
-                description: 'Postman introduction'
-            }, {
-                link: 'https://javascript.info/strict-mode',
-                text: 'JavaScript Info',
-                description: 'Javascrip programming info'
-            }, {
-                link: 'https://www.knime.com/',
-                text: 'KNIME',
-                description: 'Open source ETL and much more'
-            }]
+                    link: 'https://www.udemy.com/the-complete-nodejs-developer-course-2/',
+                    text: 'Udemy, NodeJS developer course',
+                    description: 'odeJS interactive training with teachter. <br> This training was purchased in October 2018.'
+                }, {
+                    link: 'https://www.npmjs.com/',
+                    text: 'npm',
+                    description: 'Library containing thousents of open source building blocks. <br> These blocks are free to use in your application.'
+                }, {
+                    link: 'http://expressjs.com/',
+                    text: 'express',
+                    description: 'Library facilitating webserver framework. <br> Extensive learning, help and documentation can be found on http://expressjs.com/'
+                }, {
+                    link: 'http://handlebarsjs.com/',
+                    text: 'Handlebars',
+                    description: 'Library for dynamic html (npm install hbs)'
+                }, {
+                    link: 'https://medium.freecodecamp.org/the-complete-javascript-handbook-f26b2c71719c',
+                    text: 'Complete JavaScript handbook',
+                    description: 'Free online book about JavaScript.'
+                }, {
+                    link: 'https://help.github.com/',
+                    text: 'GitHub Help website',
+                    description: 'Free online book about JavaScript.'
+                }, {
+                    link: 'https://mongodb.github.io/node-mongodb-native/',
+                    text: 'MongoDB',
+                    description: 'MongoDB and native MongoDB driver for NodeJS'
+                }, {
+                    link: 'https://mongoosejs.com/',
+                    text: 'Mongoose',
+                    description: 'Library for mongodb data manipulation for NodeJS'
+                }, {
+                    link: 'https://dashboard.heroku.com',
+                    text: 'Heroku',
+                    description: 'To publish NodeJs websites'
+                }, {
+                    link: 'https://github.com/',
+                    text: 'Github',
+                    description: 'To publish NodeJs websites'
+                }, {
+                    link: 'https://www.w3schools.com/howto/howto_js_full_page_tabs.asp',
+                    text: 'w3school',
+                    description: 'Website with all kid of web design reference materials'
+                }, {
+                    link: 'https://www.getpostman.com/',
+                    text: 'postman',
+                    description: 'Postman is a toolbox for testing and monitoring API\'s.'
+                }, {
+                    link: "https://httpstatuses.com/",
+                    text: 'https statuses',
+                    description: 'All https statuses that exist'
+                }, {
+                    link: 'http://blog.getpostman.com/2018/04/11/first-5-things-to-try-if-youre-new-to-postman/?mkt_tok=eyJpIjoiTnpZeE4ySTRObVV6WWpNdyIsInQiOiJZUEtrQnFtVjRCcTdsWllTVXN2ZmhnMFc2cWhGNjZqdmtQRlduRGZmeEpYTTRGTGxxa0N6T3IraEVGSUF3d3g0RW5cL1FYbUNuUUh5Sm5OaFJyWjNoSG9vOHFVT2xYalR4SjY4MmdrS0pQMG5JUmpKMlplSDBBSDQxdXRjUFg0ZzAifQ%3D%3D',
+                    text: 'Try Postmans, 5 things to try out.',
+                    description: 'Postman introduction'
+                }, {
+                    link: 'https://javascript.info/strict-mode',
+                    text: 'JavaScript Info',
+                    description: 'Javascrip programming info'
+                }, {
+                    link: 'https://www.knime.com/',
+                    text: 'KNIME',
+                    description: 'Open source ETL and much more'
+                }, {
+                    link: 'https://hackernoon.com/beginning-your-web-development-journey-start-here-7084346f5762',
+                    text: 'hackernoon.com - Beginning your web development journey',
+                    description: 'Article referring to fundamental webdevelopment training. <br> The following links refer to courses referred to in this article. <br> Principal message <b>Learn HTML, CSS, and Javascript exclusively.</b>'
+                }, {
+                    link: 'https://medium.freecodecamp.org/',
+                    text: 'Programmer articles',
+                    description: 'Read this to get into the Community'
+                }, {
+                    link: 'https://hackernoon.com/',
+                    text: 'Programmer articles',
+                    description: 'Read this to get into the Community'
+                }, {
+                    link: 'https://twitter.com',
+                    text: 'Follow these guys on twitter for more specialised info.',
+                    description: 'Caldera (shameless plug), Dan Abramov, Kyle Mathews, Christian Nwamba, Wes Bos, Jason Miller 🦊 ⚛, Scott Tolinski, Ryan Florence'
+                }, {
+                    link: 'https://www.google.com/search?q=html+training+online&ie=utf-8&oe=utf-8&client=firefox-b',
+                    text: 'HTML and CSS',
+                    description: 'Do not start learning libraries like Bootstrap start with plain HTML and CSS <br> https://www.codecademy.com/ <br> https://teamtreehouse.com/tracks/front-end-web-development'
+                }, {
+                    link: 'https://teamtreehouse.com/library/git-basics',
+                    text: 'Git basics',
+                    description: 'Things about GIT you must know.'
+                }
+
+            ]
         });
     } catch (error) {
         console.log(error);
